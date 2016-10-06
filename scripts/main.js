331fb9e52aea42010fb11894844bb85aec70ce7d
Willyson Eveiro <go.willyson@gmail.com>
2016-10-06 23:21:15 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+//Clickable images
+
+var myImage= document.querySelector ('img');
+
+myImage.onclick = function () {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/wth.jpg') {
+	myImage.setAttribute ('src','images/abt.jpg');
+	} else {
+		myImage.setAttribute ('src','images/wth.jpg');
+	}
+}
+
+//Personalized title
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name',myName);
+	myHeading.textContent= 'Something by Willyson for, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Something by Willyson for, '+ storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
